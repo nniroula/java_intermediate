e92975cf298e7d9444943f4e133d84af736e57e9
nabin <nkbhaie2017@gmail.com>
2024-04-15 19:18:15 -0600

    add HashMap creation

--- maps/HashMaps.java
+++ maps/HashMaps.java
@@ -1,5 +1,37 @@
 package maps;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * To run this program in CLI
+ *  javac -d . HashMaps.java
+ *  java maps.HashMaps
+ */
+
+/*
+ * HashMap takes key value pair. It's like object in JS and dictionary in Python
+ */
+
+class KeyValuePair{
+    /*
+     * Static method could be directly called by its class in main method
+    */
+
+    public static Map<String, String> getHashMap(){
+        Map<String, String> hsMap = new HashMap<>();
+        hsMap.put("Java", "Spring Boot");
+        hsMap.put("JS", "ExpresJS");
+        hsMap.put("Python", "Flask");
+        return hsMap;
+    }
+}
+
+
 public class HashMaps {
+    public static void main(String []args){
+        //getHashMap method
+        System.out.println("getHashMap function's output is ...\n" + KeyValuePair.getHashMap());
+    }
     
 }
