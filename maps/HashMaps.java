964d414d8f105ff9b6b7f85c78bdc7376a79d895
nabin <nkbhaie2017@gmail.com>
2024-04-15 19:34:36 -0600

    add hash map methods of isEmpty and put elements

--- maps/HashMaps.java
+++ maps/HashMaps.java
@@ -18,6 +18,9 @@ class KeyValuePair{
      * Static method could be directly called by its class in main method
     */
 
+    // create a global HashMap
+    private static Map<String, Integer> globalHashMap = new HashMap<>();
+
     public static Map<String, String> getHashMap(){
         Map<String, String> hsMap = new HashMap<>();
         hsMap.put("Java", "Spring Boot");
@@ -25,13 +28,50 @@ class KeyValuePair{
         hsMap.put("Python", "Flask");
         return hsMap;
     }
+
+    /*
+     * addElement method
+     */
+    public static Map<String, String> addElement(){
+        Map<String, String> newHashMap = new HashMap<>();
+        newHashMap.put("JS", "Front End");
+        newHashMap.put("Java", "BackEnd language");
+        return newHashMap;
+    }
+
+    /*
+     * isEmpty method
+    */
+    public static boolean isHashMapEmpty(){
+        return globalHashMap.isEmpty();
+    }
+
+    //size() method
+
+    // contains();
+
+    // remove method
+
+    // print out all keys
+
+    // pirnt out all values
+
 }
 
 
 public class HashMaps {
     public static void main(String []args){
-        //getHashMap method
+        //getHashMap method returns HashMap
         System.out.println("getHashMap function's output is ...\n" + KeyValuePair.getHashMap());
+   
+        //addElement method
+        System.out.println();
+        System.out.println("addElement method's output is ... \n" + KeyValuePair.addElement());
+        
+        //isHashMapEmpty method
+        System.out.println();
+        System.out.println("isHashMapEmpty method's output is ... \n" + KeyValuePair.isHashMapEmpty());
+    
     }
     
 }
