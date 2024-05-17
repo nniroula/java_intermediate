fc8426bce8a5b12a3782244b3e5994ecad91d272
nabin <nkbhaie2017@gmail.com>
2024-05-17 11:03:16 -0600

    add an interface

---
+++ multipleClasses/InterfaceConsole.java
@@ -0,0 +1,10 @@
+package multipleClasses;
+
+public interface InterfaceConsole {
+    // all methods are public and static. Static means they are class methods
+    void getGameType();
+    void left();
+    void right();
+    void up();
+    void down();
+}
---
+++ multipleClasses/SuperContra.java
@@ -0,0 +1,26 @@
+package multipleClasses;
+
+public class SuperContra implements InterfaceConsole{
+    public void getGameType(){
+        System.out.println("SuperContra Game class...");
+    }
+     // left button 
+     public void left(){
+        System.out.println("LEFT");
+    }
+
+    // right button
+    public void right(){
+        System.out.println("RIGHT");
+    }
+
+    // up button
+    public void up(){
+        System.out.println("UP");
+    }
+
+    // down button
+    public void down(){
+        System.out.println("DOWN");
+    }
+}
